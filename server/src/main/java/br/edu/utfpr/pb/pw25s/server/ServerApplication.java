61dd996055a3cb4adbac86a0bda5a61bd8d6e620
cenci-ntido <105450250+cenci-ntido@users.noreply.github.com>
2023-11-26 18:07:37 -0300

    teste

--- server/src/main/java/br/edu/utfpr/pb/pw25s/server/ServerApplication.java
+++ server/src/main/java/br/edu/utfpr/pb/pw25s/server/ServerApplication.java
@@ -2,7 +2,7 @@ package br.edu.utfpr.pb.pw25s.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//teste
 @SpringBootApplication
 public class ServerApplication {
 
