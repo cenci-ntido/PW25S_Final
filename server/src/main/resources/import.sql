cbf397b2125ee4acf6b1e06b6b00cc6b725360e3
cenci-ntido <105450250+cenci-ntido@users.noreply.github.com>
2023-12-02 10:15:39 -0300

    Tudo certo

--- server/src/main/resources/import.sql
+++ server/src/main/resources/import.sql
@@ -1,5 +1,3 @@
-INSERT INTO tb_user(display_name, username, password) VALUES ('Administrador', 'admin','$2a$10$.PVIfB07x.SfMYTcToxL0.yxcLWU0GbS2NUO1W1QAvqMm/TsFhVem');
-INSERT INTO tb_user(display_name, username, password) VALUES ('Teste', 'test','$2a$10$.PVIfB07x.SfMYTcToxL0.yxcLWU0GbS2NUO1W1QAvqMm/TsFhVem');
+INSERT INTO tb_user(display_name, username, password) VALUES ('adasadsadsad','dasaddsasadadsas','$2a$10$1c3hBHL72n6bGBLk1QnFk.btKisaGsp6AmXtZztmzBJI3DBz/hkNq');
 INSERT INTO tb_account (saved_money, user_id, description) VALUES(1000, 1, 'Nubank');
-INSERT INTO tb_account (saved_money, user_id, description) VALUES(100, 2, 'Nubank');
 INSERT INTO tb_account (saved_money, user_id, description) VALUES(0, 1, 'Inter');
